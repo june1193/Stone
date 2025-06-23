df3d5ac66528094017e99edd1bfe17fb61f852eb
유원준 <june1193@entropykorea.com>
2025-06-23 17:43:46 +0900

    리액트 다운그레이드 19 -> 18

--- stone-front/src/App.jsx
+++ stone-front/src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css'
-import BasicDataGrid from "./Component/BasicDataGrid.jsx";
+import { LicenseInfo } from "@mui/x-license-pro";
+import { MUI_LICENSE_KEY } from "./license";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./View/Home.jsx";
+import AuthByMenuManage from './View/Standard/AuthByMenuManage.jsx';
+
 
 function App() {
+    LicenseInfo.setLicenseKey(MUI_LICENSE_KEY);
+    /*
+    @mui/x-data-grid-pro 6.x 버전은 React 17, 18만 지원
+    현재 프로젝트는 React 19 사용 중이라 지원 안됨 -> 리엑트 다운그레이드
+    */
 
   return (
-      <div>
-          <h1>Hello</h1>
-          <BasicDataGrid></BasicDataGrid>
-      </div>
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/Pagetwo" element={<AuthByMenuManage />} />
+          </Routes>
+      </Router>
   )
 }
 
