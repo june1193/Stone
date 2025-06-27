ae54d65fb0c6fd5e5215fd01a17194d5309367b7
유원준 <june1193@entropykorea.com>
2025-06-27 11:23:29 +0900

    홈버튼 생성

--- stone-front/src/View/Home.jsx
+++ stone-front/src/View/Home.jsx
@@ -5,9 +5,9 @@ function Home() {
         <div>
             <h1>홈</h1>
             <ul>
-                <li><Link to="/file-test">📁 파일 생성 테스트</Link></li>
-                <li><Link to="/grid-test">📊 그리드 테스트</Link></li>
-                <li><Link to="/chart-test">📈 차트 테스트</Link></li>
+                <li><Link to="/file-test">파일 생성 테스트</Link></li>
+                <li><Link to="/grid-test">그리드 테스트</Link></li>
+                <li><Link to="/chart-test">차트 테스트</Link></li>
             </ul>
         </div>
     );
--- stone-front/src/View/Skills/File.jsx
+++ stone-front/src/View/Skills/File.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom"; // 추가
 
 function File() {
     const [fileName, setFileName] = useState("test");
     const [readYm, setReadYm] = useState("202406");
     const [message, setMessage] = useState("");
+    const navigate = useNavigate(); // 추가
 
     const handleGenerate = async () => {
         try {
             const res = await axios.get("http://localhost:8081/api/file/generate", {
                 params: { fileName, readYm },
             });
-            setMessage(res.data); // 성공 시: 파일 경로 메시지
+            setMessage(res.data);
         } catch (err) {
             setMessage("에러: " + err.message);
         }
@@ -29,6 +31,12 @@ function File() {
                 <input value={readYm} onChange={(e) => setReadYm(e.target.value)} />
             </div>
             <button onClick={handleGenerate}>파일 생성</button>
+
+            {/* 홈으로 가기 버튼 */}
+            <button onClick={() => navigate("/")} style={{ marginLeft: "1em" }}>
+                홈으로
+            </button>
+
             <div style={{ marginTop: "1em" }}>{message}</div>
         </div>
     );
