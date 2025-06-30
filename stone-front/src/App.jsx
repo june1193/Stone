226ad8de343a2b6aff47c32cc44b730dd7acb261
유원준 <june1193@entropykorea.com>
2025-06-30 16:52:21 +0900

    그리드 버전 다운그레이드

--- stone-front/src/App.jsx
+++ stone-front/src/App.jsx
@@ -10,10 +10,6 @@ import Chart from './View/Skills/Chart.jsx';
 
 function App() {
     LicenseInfo.setLicenseKey(MUI_LICENSE_KEY);
-    /*
-    @mui/x-data-grid-pro 6.x 버전은 React 17, 18만 지원
-    현재 프로젝트는 React 19 사용 중이라 지원 안됨 -> 리엑트 다운그레이드
-    */
 
   return (
       <Router>
